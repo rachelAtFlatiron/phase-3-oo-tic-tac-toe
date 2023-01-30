96b0f71e7617c6223b8ccf874d28cd3bc92c6eb9
Rachel Katz <rachel.katz@flatironschool.com>
2023-01-30 14:09:38 -0500

    commit tree f3ac9078c39382ccfb7487a00b24e3e57fc0ea38

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,46 @@
+class TicTacToe
+    
+    WIN_COMBINATIONS = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+
+    def initialize
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
+    end
+
+    def input_to_index(space)
+        space.to_i - 1
+    end 
+
+    def move(space, token='X')
+        @board[space] = token
+    end
+
+    def position_taken?(index)
+        @board[index] != " "
+    end
+
+    def valid_move?(space)
+        !position_taken?(space) && space.between?(0,8)
+    end
+
+    def turn_count 
+        @board.count do |el|
+            el != ' '
+        end 
+    end 
+
+    def current_player
+        (turn_count % 2 != 0) ? 'O' : 'X'
+    end 
+end 
